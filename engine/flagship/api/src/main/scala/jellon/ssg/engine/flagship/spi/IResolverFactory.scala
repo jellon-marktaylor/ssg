3e21f58e4ff62e424489a3a1e9342d5a69039097
jellon-marktaylor <jellon.marktaylor@gmail.com>
2022-06-17 16:53:14 -0400

    ResolverFactory can take an optional parameter to pick the type of resolver it provides

--- engine/flagship/api/src/main/scala/jellon/ssg/engine/flagship/spi/IResolverFactory.scala
+++ engine/flagship/api/src/main/scala/jellon/ssg/engine/flagship/spi/IResolverFactory.scala
@@ -5,26 +5,51 @@ import jellon.ssg.node.api.{INode, INodeMap}
 object IResolverFactory {
 
   implicit class IResolverFactoryExt(self: IResolverFactory) {
+    def resolver(dictionary: INodeMap): IResolver =
+      self.defaultResolver(dictionary)
+
+    def resolver(name: String, dictionary: INodeMap): IResolver =
+      self.namedResolver(name, dictionary)
+
+    def asNode(name: String, dictionary: INodeMap, rawText: String): INode =
+      self.namedResolver(name, dictionary).asNode(rawText)
+
+    def asNode(name: String, dictionary: INodeMap, rawNode: INode): INode =
+      self.namedResolver(name, dictionary).asNode(rawNode)
+
+    def asText(name: String, dictionary: INodeMap, rawText: String): String =
+      self.namedResolver(name, dictionary).asText(rawText)
+
+    /** if rawNode is a ValueNode(string) then we return Node(asText(string)) else we return rawNode */
+    def resolveIfStringNode(name: String, dictionary: INodeMap, node: INode): INode =
+      self.namedResolver(name, dictionary).resolveIfStringNode(node)
+
+    /** call [[resolveIfStringNode(INode)]] on each node in the NodeMap. As a result, each ValueNode(string) will be resolved if it contained a template */
+    def resolveStringAttributes(name: String, dictionary: INodeMap, node: INodeMap): INodeMap =
+      self.namedResolver(name, dictionary).resolveStringAttributes(node)
+
     def asNode(dictionary: INodeMap, rawText: String): INode =
-      self.resolver(dictionary).asNode(rawText)
+      self.defaultResolver(dictionary).asNode(rawText)
 
     def asNode(dictionary: INodeMap, rawNode: INode): INode =
-      self.resolver(dictionary).asNode(rawNode)
+      self.defaultResolver(dictionary).asNode(rawNode)
 
     def asText(dictionary: INodeMap, rawText: String): String =
-      self.resolver(dictionary).asText(rawText)
+      self.defaultResolver(dictionary).asText(rawText)
 
     /** if rawNode is a ValueNode(string) then we return Node(asText(string)) else we return rawNode */
     def resolveIfStringNode(dictionary: INodeMap, node: INode): INode =
-      self.resolver(dictionary).resolveIfStringNode(node)
+      self.defaultResolver(dictionary).resolveIfStringNode(node)
 
     /** call [[resolveIfStringNode(INode)]] on each node in the NodeMap. As a result, each ValueNode(string) will be resolved if it contained a template */
     def resolveStringAttributes(dictionary: INodeMap, node: INodeMap): INodeMap =
-      self.resolver(dictionary).resolveStringAttributes(node)
+      self.defaultResolver(dictionary).resolveStringAttributes(node)
   }
 
 }
 
 trait IResolverFactory {
-  def resolver(dictionary: INodeMap): IResolver
+  def defaultResolver(dictionary: INodeMap): IResolver
+
+  def namedResolver(name: String, dictionary: INodeMap): IResolver
 }
--- engine/flagship/guice/src/main/scala/jellon/ssg/engine/flagship/modules/FlagshipEngineModule.scala
+++ engine/flagship/guice/src/main/scala/jellon/ssg/engine/flagship/modules/FlagshipEngineModule.scala
@@ -3,8 +3,7 @@ package jellon.ssg.engine.flagship.modules
 import jellon.ssg.engine.flagship.api.IFlagshipApplication
 import jellon.ssg.engine.flagship.integ.FlagshipHintHandlers
 import jellon.ssg.engine.flagship.spi.IResolverFactory
-import jellon.ssg.engine.flagship.st4.ST4ResolverFactory
-import jellon.ssg.engine.flagship.{AbstractGuiceModule, FlagshipApplication}
+import jellon.ssg.engine.flagship.{AbstractGuiceModule, FlagshipApplication, ResolverFactory}
 import jellon.ssg.io.spi.IHintHandlers
 import jellon.ssg.node.parser.api.INodeParsers
 import jellon.ssg.node.parser.impl.DefaultNodeParsers
@@ -17,6 +16,6 @@ object FlagshipEngineModule extends AbstractGuiceModule {
     bindTo[INodeParsers, DefaultNodeParsers]
     // engine
     bindTo[IFlagshipApplication, FlagshipApplication]
-    bindTo[IResolverFactory](() => ST4ResolverFactory)
+    bindTo[IResolverFactory](() => ResolverFactory)
   }
 }
