19c38db9b17eb7b5cb340756c74171b6ec557d62
jellon-marktaylor <jellon.marktaylor@gmail.com>
2022-06-18 04:37:12 -0400

    adding support for st4 -> ST4Resolver, velocity -> VelocityResolver to ResolverFactory

--- engine/flagship/impl/src/main/scala/jellon/ssg/engine/flagship/ResolverFactory.scala
+++ engine/flagship/impl/src/main/scala/jellon/ssg/engine/flagship/ResolverFactory.scala
@@ -2,12 +2,20 @@ package jellon.ssg.engine.flagship
 
 import jellon.ssg.engine.flagship.spi.{IResolver, IResolverFactory}
 import jellon.ssg.engine.flagship.st4.ST4Resolver
+import jellon.ssg.engine.flagship.velocity.VelocityResolver
 import jellon.ssg.node.api.INodeMap
+import jellon.ssg.node.spi.MapNode
 
 object ResolverFactory extends IResolverFactory {
   override def defaultResolver(dictionary: INodeMap): IResolver =
     new ST4Resolver(dictionary)
 
-  override def namedResolver(name: String, dictionary: INodeMap): IResolver =
-    defaultResolver(dictionary)
+  override def namedResolver(name: String, dictionary: INodeMap): IResolver = name match {
+    case "st4" =>
+      new ST4Resolver(dictionary)
+    case "velocity" =>
+      new VelocityResolver(new MapNode(dictionary))
+    case _ =>
+      defaultResolver(dictionary)
+  }
 }
